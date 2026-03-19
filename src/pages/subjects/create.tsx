ae800a1a7843401ee269bb0d1d4f294a128060fa
kxxian <kristianryanbulos@gmail.com>
2026-03-19 15:32:40 +0800

    initial commit

---
+++ src/pages/subjects/create.tsx
@@ -0,0 +1,9 @@
+const SubjectsCreate = () => {
+  return (
+    <div>
+      <h1>create</h1>
+    </div>
+  );
+};
+
+export default SubjectsCreate;
---
+++ src/pages/subjects/list.tsx
@@ -0,0 +1,9 @@
+const SubjectList = () => {
+  return (
+    <div>
+      <h1>List</h1>
+    </div>
+  );
+};
+
+export default SubjectList;
